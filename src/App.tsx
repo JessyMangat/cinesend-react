bda0d36bdf7960cdcf2fa6f84abfe4c7397fe6fa
Jessy <jessymangat22@hotmail.com>
2022-07-31 11:06:58 -0400

    initial project scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import './App.css';
+import HomePage from './components/pages/HomePage';
+import NotFoundPage from './components/pages/NotFound';
+
+const queryClient = new QueryClient()
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/*' element={<NotFoundPage />} />
+        </Routes>
+      </QueryClientProvider>
+
+    </BrowserRouter>
   );
 }
 
